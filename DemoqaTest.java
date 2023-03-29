e987c6208d7415e9cbc0730f4a174e405b053e9c
Nagore54 <olgachayukova@gmail.com>
2023-03-29 22:44:06 +0700

    Edit3 file

--- DemoqaTest.java
+++ DemoqaTest.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.io.File;
 import java.util.concurrent.TimeUnit;
 
@@ -17,30 +16,32 @@ public class DemoqaTest {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://demoqa.com/automation-practice-form");
+
         WebElement selectAll = driver.findElement(By.xpath("//*[@id='submit']"));
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", selectAll);
+
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
     }
 
     @Test
     public void registrationForm() {
-        String Username = "Vladislav";
-        String Lastname = "Lantratov";
+        String username = "Vladislav";
+        String lastname = "Lantratov";
         String fullname = "Vladislav Lantratov";
         String email = "VladLantratov@gmail.com";
         String number = "1234567895";
         String address = "ulitca Pushkina dom Kolotushkina";
         String gender = "Female";
-        String DateofBirth = "10 April,1990";
+        String dateOfBirth = "10 April,1990";
         String subjects = "Chemistry";
         String hobbies = "Sports, Music";
-        String StateandCity = "Uttar Pradesh Lucknow";
-        File file = new File("C:\\Users\\olgac\\Pictures\\sdcard\\BadCom.jpg");
+        String stateAndCity = "Uttar Pradesh Lucknow";
+        File file = new File("./BadCom.jpg");
         String photo = file.getAbsolutePath();
         String picture = "BadCom.jpg";
 
-        driver.findElement(By.xpath("//*[@id='firstName']")).sendKeys(Username);
-        driver.findElement(By.xpath("//*[@id='lastName']")).sendKeys(Lastname);
+        driver.findElement(By.xpath("//*[@id='firstName']")).sendKeys(username);
+        driver.findElement(By.xpath("//*[@id='lastName']")).sendKeys(lastname);
         driver.findElement(By.xpath("//*[@id='userEmail']")).sendKeys(email);
         driver.findElement(By.xpath("//*[@id='genterWrapper']/div[2]/div[2]/label")).click();
         driver.findElement(By.xpath("//*[@id='userNumber']")).sendKeys(number);
@@ -82,11 +83,11 @@ public class DemoqaTest {
         Assertions.assertTrue(driver.findElement(By.xpath("//*[@class='modal-body']/div/table/tbody/tr[2]/td[2]")).getText().contains(email));
         Assertions.assertTrue(driver.findElement(By.xpath("//*[@class='modal-body']/div/table/tbody/tr[3]/td[2]")).getText().contains(gender));
         Assertions.assertTrue(driver.findElement(By.xpath("//*[@class='modal-body']/div/table/tbody/tr[4]/td[2]")).getText().contains(number));
-        Assertions.assertTrue(driver.findElement(By.xpath("//*[@class='modal-body']/div/table/tbody/tr[5]/td[2]")).getText().contains(DateofBirth));
+        Assertions.assertTrue(driver.findElement(By.xpath("//*[@class='modal-body']/div/table/tbody/tr[5]/td[2]")).getText().contains(dateOfBirth));
         Assertions.assertTrue(driver.findElement(By.xpath("//*[@class='modal-body']/div/table/tbody/tr[6]/td[2]")).getText().contains(subjects));
         Assertions.assertTrue(driver.findElement(By.xpath("//*[@class='modal-body']/div/table/tbody/tr[7]/td[2]")).getText().contains(hobbies));
         Assertions.assertTrue(driver.findElement(By.xpath("//*[@class='modal-body']/div/table/tbody/tr[8]/td[2]")).getText().contains(picture));
         Assertions.assertTrue(driver.findElement(By.xpath("//*[@class='modal-body']/div/table/tbody/tr[9]/td[2]")).getText().contains(address));
-        Assertions.assertTrue(driver.findElement(By.xpath("//*[@class='modal-body']/div/table/tbody/tr[10]/td[2]")).getText().contains(StateandCity));
+        Assertions.assertTrue(driver.findElement(By.xpath("//*[@class='modal-body']/div/table/tbody/tr[10]/td[2]")).getText().contains(stateAndCity));
     }
 }
